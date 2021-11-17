dced74fbbd0f5e4e2a15f88844ee698b7b211a5d
Dmitriy Melnichenko <melnichenko.dmytro@gmail.com>
2021-11-18 01:54:56 +0200

    Implement input handling and button click handling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import { useRef, useState } from 'react';
+import './styles/App.css';
 
 function App() {
+
+  const [isDisabled, setIsDisabled] = useState(false);
+  const inputRef = useRef();
+
+  function handleInput() {
+    inputRef.current.value.toLowerCase() === 'react' ? setIsDisabled(true) : setIsDisabled(false);
+  }
+
+  function handleFocusClick() {
+    inputRef.current.focus();
+  }
+
   return (
     <div className="App">
+      <input type="text" ref={inputRef} onChange={handleInput}></input>
+      <button disabled={isDisabled}>Disable Me!</button>
+      <button onClick={handleFocusClick}>Now Focus</button>
     </div>
   );
 }
